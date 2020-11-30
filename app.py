18c7acbc543b63aaec5fd04b2d8ebb0d87558e86
Guido van Rossum <guido@python.org>
2020-11-30 09:46:40 -0800

    Add routes /favicon.ico and /hello

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
+import sys
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
+def main():
+    with open("pylabs.html") as f:
+        return f.read()
+
+@app.route("/favicon.ico")
+def favicon():
+    with open("favicon.ico", "rb") as fb:
+        return fb.read()
+
+@app.route("/hello")
 def hello():
-    return "Hello, World!"
+    return "Hello, world."
